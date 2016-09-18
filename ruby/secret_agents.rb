159c60ad7de403b79807888f6a095e51da141ee5
Alex Giacobbe <agiacobbe18@gmail.com>
2016-09-18 15:25:26 -0400

    modify secret_agents

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,17 @@
+=begin
+1. Ask user for a string:
+2. Move each letter in string one letter forward.	
+=end
+
+puts 'Welcome to Secret Agent Message Encryption'
+puts 'Input the message you wish to be encrypted:'
+
+message_to_encrypt = gets.chomp.downcase
+	puts message_to_encrypt
+
+def encrypt (message)
+
+	
+	
+end
+ 
