365618c9cfc12904b31c3975a1367921c5f5ce28
Alex Giacobbe <agiacobbe18@gmail.com>
2016-09-24 11:41:08 -0400

    Release 0

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,25 @@
+#Release 0
+#write a method that takes an array of integers and an array to search for.
+#return the index of the item
+#return nil if integer is not present
+
+
+def search_int(integer, array)
+	counter = 0
+	array.each do |arr| 
+		if arr == integer 
+			return counter
+		else 
+			return nil
+
+		end
+			counter +=1	
+	end
+end
+
+
+puts "What is the number you're looking for?"
+number = gets.chomp.to_i
+
+array = [19,17,11,23,34,56,59,48,45,55]
+p search_int(number, array)
