c4e4080d3634b6156615f7b3193f91b6ea80c19e
Alex Giacobbe <agiacobbe18@gmail.com>
2016-09-24 12:46:19 -0400

    Release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -23,3 +23,20 @@ number = gets.chomp.to_i
 
 array = [19,17,11,23,34,56,59,48,45,55]
 p search_int(number, array)
+
+#Release 1
+
+def fib(x)
+	counter = (x - 2)
+	counter.times.each_with_object([0,1]) do |num,obj| obj << obj [-2] + obj[-1]
+	end
+end
+
+p fib(6)
+
+	if fib(100).last == 218922995834555169026
+		puts "True"
+	else
+		puts "False"
+end
+
