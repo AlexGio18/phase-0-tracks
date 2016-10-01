604aaee917c0a10f22cec56c01970bfbd7e355c5
Alex Giacobbe <agiacobbe18@gmail.com>
2016-09-30 20:38:27 -0400

    Release 1:Release 1: Give Santa Attributes for Christmas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,12 @@
+#CLASS CODE ------------------------->
 class Santa
+	def initialize(name, gender, ethnicity)
+		@name = name
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -9,14 +17,21 @@ class Santa
 		cookie_type
 	end
 
-	def initialize
-		puts "Initializing Santa instance ..."
-	end
+	
 end
 
-santa_class = Santa.new
+santas = []
+example_names = ["Carston", "Vida","Miguela","Bob","Cath","Tysson","Gauté"]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+	
 
+example_genders.each_index do |i|
+	santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i])
+end
+
+santas
 #DRIVER CODE ------------------------->
+p santas[1]
+p santas[2,3]
 
-p santa_class.speak
-p santa_class.eat_milk_and_cookies("ginger bread")
