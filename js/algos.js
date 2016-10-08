9874a3bfcaf313a6636c998f97318258d3db3be2
Alex Giacobbe <agiacobbe18@gmail.com>
2016-10-08 17:46:47 -0400

    Pseudocode for Release 1

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-/*PSEUDOCODE ------------------->
+/*PSEUDOCODE: RELEASE 0 ------------------->
 Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
 Create a counter that loops through each item in the array.
 For each of these items, we need to find the length of chracters for each.
@@ -23,3 +23,11 @@ for(var i = 0; i < array.length; i++){
 var wordArray = ['monkey','harold','bobbit','turkey and chicken','sandwich'];
 
 findLongest(wordArray)
+
+/*PSEUDOCODE: RELEASE 0 ------------------->
+Write a function that takes two objects and checks to see if the objects share 
+at least one key value pair.
+create a boolean statement that compares the key value pairs of two hashy-type data
+structures. 
+If at least one of the key/value pairs from each object match, return a true statement.
+*/
