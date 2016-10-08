4977462a6b9b3e759fbaf5ce59eba691eb912e53
Alex Giacobbe <agiacobbe18@gmail.com>
2016-10-08 17:39:04 -0400

    Release 0 create a function that finds the longest phrase in an array

--- js/algos.js
+++ js/algos.js
@@ -6,3 +6,20 @@ As the counter goes through each item, look for the longest phrase or word.
 The longest word is compared to the next in the index, and if it is shorter, gets replaced. Keeps iterating through till the end until the longest is left.
 return the longest phrase or word to the console.
 */
+
+function findLongest(array){
+var longestWord = " ";
+for(var i = 0; i < array.length; i++){
+	if(array[i].length > longestWord.length){
+		longestWord = array[i];
+	}
+
+}
+	console.log(longestWord);
+}
+
+/* RELEASE 0: DRIVER CODE */
+
+var wordArray = ['monkey','harold','bobbit','turkey and chicken','sandwich'];
+
+findLongest(wordArray)
