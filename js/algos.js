de1b714acc159ee3bdb91fdfd0e0b1bd511641ee
Alex Giacobbe <agiacobbe18@gmail.com>
2016-10-08 17:29:53 -0400

    added pseudocode for longest phrase (Release 0)

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+/*PSEUDOCODE ------------------->
+Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+Create a counter that loops through each item in the array.
+For each of these items, we need to find the length of chracters for each.
+As the counter goes through each item, look for the longest phrase or word.
+The longest word is compared to the next in the index, and if it is shorter, gets replaced. Keeps iterating through till the end until the longest is left.
+return the longest phrase or word to the console.
+*/
