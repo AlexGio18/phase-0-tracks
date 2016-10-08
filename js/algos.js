9ac2666b4743d94554f55eca0d37bb649b8046ab
Alex Giacobbe <agiacobbe18@gmail.com>
2016-10-08 19:42:09 -0400

    Release 1 create function that finds a key-value match

--- js/algos.js
+++ js/algos.js
@@ -7,6 +7,7 @@ The longest word is compared to the next in the index, and if it is shorter, get
 return the longest phrase or word to the console.
 */
 
+//RELEASE 0:FUNCTION ------------------->
 function findLongest(array){
 var longestWord = " ";
 for(var i = 0; i < array.length; i++){
@@ -18,16 +19,42 @@ for(var i = 0; i < array.length; i++){
 	console.log(longestWord);
 }
 
-/* RELEASE 0: DRIVER CODE */
-
-var wordArray = ['monkey','harold','bobbit','turkey and chicken','sandwich'];
-
-findLongest(wordArray)
-
-/*PSEUDOCODE: RELEASE 0 ------------------->
+/*PSEUDOCODE: RELEASE 1 ------------------->
 Write a function that takes two objects and checks to see if the objects share 
 at least one key value pair.
 create a boolean statement that compares the key value pairs of two hashy-type data
 structures. 
 If at least one of the key/value pairs from each object match, return a true statement.
 */
+
+//RELEASE 1:FUNCTION ------------------->
+function match_hashy(object1, object2) {
+  var match = false;
+  for (var i in object1) {
+     if (object2.hasOwnProperty(i)) {
+      if (object1[i] == object2[i]) {
+         match = true;
+        }
+      }
+    }
+    console.log(match);
+  }
+
+/* RELEASE 0: DRIVER CODE */
+
+// var wordArray = ['monkey','harold','bobbit','turkey and chicken','sandwich'];
+
+// findLongest(wordArray)
+
+
+/* RELEASE 1: DRIVER CODE */
+
+var hashyType1 = {name: "Darboo", favphrase: "Kentucky Fried Rice"};
+var hashyType2 = {name: "Darboo Jr.", favphrase: "Kentucky Fried Rice"};
+
+match_hashy(hashyType1,hashyType2);
+
+var newHashy = {name: "Katinka", favphrase:"Better out that in, I alway say!"};
+var newHashy2 = {name: "Crosby", favphrase:"Insignias... Insignias"};
+
+match_hashy(newHashy, newHashy2);
