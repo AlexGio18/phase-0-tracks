c9f62836edf7978e33a1c28ea34788a8e493067a
Alex Giacobbe <agiacobbe18@gmail.com>
2016-10-09 16:01:57 -0400

    created an array that stored the hash data for each calculation, and was able to print out all calculations made at the end

--- calc-practice/calculator.rb
+++ calc-practice/calculator.rb
@@ -28,36 +28,47 @@ puts "Welcome to calculate."
 puts "Calculate ('done' to quit):"
 
 # p problem
-problem = ' '
-array = {}
-counter = 0 
+counter = 0
 
-until problem == ['done']
+array_calc = []
 
-	if problem != ['done']
-	problem = gets.chomp.split
-	n = problem[0].to_i
-	op = problem[1] 
-	i = problem[2].to_i 
+while true
+	problem = gets.chomp
 	
-	puts calculate(n,op,i)
-<<<<<<< HEAD
-=======
+	if problem == "done"
+		break
 
-	array = []
-	array << problem << "=" <<  
-	counter = 0
-	
-	while counter < array.length
-		puts 
-		puts "#{array[counter]}"
-	counter += 1
->>>>>>> 90eea416105205ed3caadbd587ebb3d5acd9deec
+	else 
+		problem_array = problem.split
+		n = problem_array[0].to_i
+		op = problem_array[1] 
+		i = problem_array[2].to_i
+		puts calculate(n,op,i)
+		
 	end
-
-
+		counter += 1 
+		calculation = calculate(n,op,i)
+		problem_array << calculation
+		hash_calc = {counter => problem_array}
+		array_calc << hash_calc
 end
+
+array_calc.each_index do |i|
+	array_calc[i].each do |key, value|
+ 	puts "Calculation #{key}: #{value[0]} #{value[1]} #{value[2]} = #{value[3]}"
+ end
 end
+
+# 	array = []
+# 	array << problem << "=" <<  
+# 	counter = 0
+	
+# 	while counter < array.length
+# 		puts 
+# 		puts "#{array[counter]}"
+# 	counter += 1
+
+# end
 # getting stuck on question 5 - trying to keep all user data and store it in a hash or array
 # and then print everything at the end.
 
