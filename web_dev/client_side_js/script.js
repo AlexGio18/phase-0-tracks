e37c6d286f175743dd71b22f53172d7bf013a890
Alex Giacobbe <agiacobbe18@gmail.com>
2016-10-19 19:14:11 -0400

    adjusted script

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,11 @@
+function addBorder(event) {
+console.log(event);
+event.target.border = "2px solid black";
+}
+
 var photo = document.getElementById("mountain-photo");
-photo.style.border = "2px solid black";
+photo.addEventListener("click", addBorder);
+
 
 // var paragraph = document.getElementsByTagName("p");
 // header.style.border = "2px solid orange";
